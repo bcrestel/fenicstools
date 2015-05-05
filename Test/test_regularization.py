067bb2c34011bb9c19a1919ef9d2b3a4f6ae357d
Benjamin Crestel <crestel@ices.utexas.edu>
2015-05-05 17:19:11 -0500

    Define and test H1 Tikhonov regularization

--- Test/test_regularization.py
+++ Test/test_regularization.py
@@ -8,52 +8,101 @@ class TestTikhonovH1(unittest.TestCase):
 
     def setUp(self):
         mesh = UnitSquareMesh(5,5,'crossed')
-        self.Vm = FunctionSpace(mesh, 'Lagrange', 1)
-        self.Regul1 = TikhonovH1({'Vm': self.Vm, 'gamma': 1.0})
+        self.Vm = FunctionSpace(mesh, 'Lagrange', 2)
+        self.m = Function(self.Vm)
+        self.m.vector()[:] = np.random.randn(self.Vm.dim())
+        self.Regulg = TikhonovH1({'Vm': self.Vm, 'gamma': 1e-5})
+        self.Regulb = TikhonovH1({'Vm': self.Vm, 'gamma': 0.0, 'beta':1e-5})
+        self.Regul = TikhonovH1({'Vm': self.Vm, 'gamma': 1e-5, \
+        'beta':1e-10, 'm0': self.m})
         self.M = []
         for ii in range(10):    self.M.append(Function(self.Vm))
         self.lenm = len(self.M[0].vector().array())
         for mm in self.M:
             mm.vector()[:] = np.random.randn(self.lenm)
-        self.m = Function(self.Vm)
 
-    def test00_inst(self):
+    def test00a_inst(self):
+        """Default instantiation and check default values"""
+        Regul = TikhonovH1({'Vm': self.Vm, 'gamma': 1e-5})
+        error = Regul.beta + np.linalg.norm(Regul.m0.vector().array())
+        self.assertTrue(error < 1e-16, error)
+
+    def test00b_inst(self):
         """Default instantiation"""
-        Regul = TikhonovH1({'Vm': self.Vm, 'gamma': 10.})
+        Regul = TikhonovH1({'Vm': self.Vm, 'gamma': 1e-5, \
+        'beta': 1e-7, 'm0': self.m})
 
     def test01_gamma(self):
         """Check gamma applied consistently"""
-        Regul10 = TikhonovH1({'Vm': self.Vm, 'gamma': 10.})
+        Regul10 = TikhonovH1({'Vm': self.Vm, 'gamma': 1e-10})
         error = 0.0
         for mm in self.M:
-            r1 = (self.Regul1.grad(mm)).array()
+            r1 = (self.Regulg.grad(mm)).array()
             r10 = (Regul10.grad(mm)).array()
-            err = np.linalg.norm(10.*r1 - r10)/np.linalg.norm(r10)
+            err = np.linalg.norm(r1 - 1e5*r10)/np.linalg.norm(r1)
             error = max(error, err)
-        self.assertTrue(error < 1e-16)
+        self.assertTrue(error < 2e-16, error)
+
+    def test01_beta(self):
+        """Check beta applied consistently"""
+        Regul10 = TikhonovH1({'Vm': self.Vm, 'gamma': 0.0, 'beta':1e-10})
+        error = 0.0
+        for mm in self.M:
+            r1 = (self.Regulb.grad(mm)).array()
+            r10 = (Regul10.grad(mm)).array()
+            err = np.linalg.norm(r1 - 1e5*r10)/np.linalg.norm(r1)
+            error = max(error, err)
+        self.assertTrue(error < 2e-16, error)
         
     def test02_getR(self):
         """Check getter for R"""
-        gR = self.Regul1.get_R()
+        gR = self.Regul.get_R()
         error = 0.0
         for mm in self.M:
-            r1 = (self.Regul1.grad(mm)).array()
+            r1 = (self.Regul.hessian(mm.vector())).array()
             r2 = (gR * mm.vector()).array()
             err = np.linalg.norm(r1 - r2)/np.linalg.norm(r1)
             error = max(error, err)
-        self.assertTrue(error < 1e-16)
+        self.assertTrue(error < 3e-16, error)
+
+    def test02_precond(self):
+        """Check preconditioner when beta is defined"""
+        prec = self.Regul.get_precond()
+        gR = self.Regul.get_R()
+        error = 0.0
+        for mm in self.M:
+            r1 = (prec * mm.vector()).array()
+            r2 = (gR * mm.vector()).array()
+            err = np.linalg.norm(r1 - r2)/np.linalg.norm(r1)
+            error = max(error, err)
+        self.assertTrue(error < 1e-16, error)
+
+    def test02_precond2(self):
+        """Check preconditioner when beta is defined"""
+        prec = self.Regulg.get_precond()
+        gR = self.Regulg.get_R()
+        gM = self.Regulg.M
+        gRM = gR + 1e-14*gM
+        error = 0.0
+        for mm in self.M:
+            r1 = (prec * mm.vector()).array()
+            r2 = (gRM * mm.vector()).array()
+            err = np.linalg.norm(r1 - r2)/np.linalg.norm(r1)
+            error = max(error, err)
+        self.assertTrue(error < 1e-16, error)
+
 
     def test03_costnull(self):
         """Check null space of regularization"""
         self.m.vector()[:] = np.ones(self.lenm)
-        cost = self.Regul1.cost(self.m) 
+        cost = self.Regulg.cost(self.m) 
         self.assertTrue(cost < 1e-16, cost)
 
     def test03_costposit(self):
         """Check cost is nonnegative"""
         mincost = 1.0
         for mm in self.M:
-            cost = self.Regul1.cost(mm)
+            cost = self.Regulg.cost(mm)
             mincost = min(mincost, cost)
         self.assertTrue(mincost > 0.0, mincost)
         
@@ -62,38 +111,46 @@ class TestTikhonovH1(unittest.TestCase):
         error = 0.0
         h = 1e-5
         for mm in self.M:
-            grad = self.Regul1.grad(mm)
+            grad = self.Regul.grad(mm)
             mm_arr = mm.vector().array()
             for dm in self.M:
                 dm_arr = dm.vector().array()
                 dm_arr /= np.linalg.norm(dm_arr)
                 gradxdm = np.dot(grad.array(), dm_arr)
                 self.m.vector()[:] = mm_arr + h*dm_arr
-                cost1 = self.Regul1.cost(self.m)
+                cost1 = self.Regul.cost(self.m)
                 self.m.vector()[:] = mm_arr - h*dm_arr
-                cost2 = self.Regul1.cost(self.m)
+                cost2 = self.Regul.cost(self.m)
                 gradxdm_fd = (cost1-cost2) / (2.*h)
                 err = abs(gradxdm - gradxdm_fd) / abs(gradxdm)
                 error = max(error, err)
-        self.assertTrue(error < 1e-7, error)
-        
+        self.assertTrue(error < 2e-7, error)
+
     def test05_hess(self):
+        """Check gradient and Hessian do the same"""
+        error = 0.0
+        for mm in self.M:
+            gradm = self.Regul.grad(mm).array()
+            hessm = self.Regul.hessian(mm.vector()-self.Regul.m0.vector())\
+            .array()
+            err = np.linalg.norm(gradm-hessm)/np.linalg.norm(gradm)
+            error = max(error, err)
+        self.assertTrue(error < 1e-16, error)
+        
+    def test06_hess(self):
         """Check gradient and hessian are consistent"""
         error = 0.0
         h = 1e-5
         for mm in self.M:
-            hess = self.Regul1.hessian(mm.vector())
-            mm_arr = mm.vector().array()
             for dm in self.M:
-                dm_arr = dm.vector().array()
-                dm_arr /= np.linalg.norm(dm_arr)
-                hessxdm = np.dot(hess.array(), dm_arr)
-                self.m.vector()[:] = mm_arr + h*dm_arr
-                grad1 = self.Regul1.grad(self.m).array()
-                self.m.vector()[:] = mm_arr - h*dm_arr
-                grad2 = self.Regul1.grad(self.m).array()
-                hessxdm_fd = (grad1-grad2) / (2.*h)
-                err = np.linalg.norm(hessxdm - hessxdm_fd) / \
-                np.linalg.norm(hessxdm)
+                Hdm = self.Regul.hessian(dm.vector()).array()
+                self.m.vector()[:] = mm.vector().array() + \
+                h*dm.vector().array()
+                G1dm = self.Regul.grad(self.m).array()
+                self.m.vector()[:] = mm.vector().array() - \
+                h*dm.vector().array()
+                G2dm = self.Regul.grad(self.m).array()
+                HFDdm = (G1dm-G2dm)/(2*h)
+                err = np.linalg.norm(HFDdm-Hdm)/np.linalg.norm(Hdm)
                 error = max(error, err)
-        self.assertTrue(error < 1e-7, error)
+        self.assertTrue(error < 1e-8, error)
--- regularization.py
+++ regularization.py
@@ -23,6 +23,9 @@ class Regularization():
     def get_R(self):    print "Needs to be implemented"
 
     @abc.abstractmethod
+    def get_precond(self):    print "Needs to be implemented"
+
+    @abc.abstractmethod
     def _assemble(self):    print "Needs to be implemented"
 
     @abc.abstractmethod
@@ -52,28 +55,44 @@ class TikhonovH1(Regularization):
     """Tikhonov regularization with H1 semi-norm
     Parameters must be a dictionary containing:
         gamma = multiplicative factor applied to H1 semi-norm
-        Vm = function space for parameter"""
+        beta = multiplicative factor applied to mass matrix (default=0.0)
+        m0 = reference medium (default to zero vector)
+        Vm = function space for parameter
+    cost = 1/2 * (m-m0)^T.R.(m-m0)"""
 
     def _assemble(self):
         self.gamma = self.Parameters['gamma']
+        if self.Parameters.has_key('beta'): self.beta = self.Parameters['beta']
+        else:   self.beta = 0.0
         self.Vm = self.Parameters['Vm']
+        if self.Parameters.has_key('m0'):   
+            self.m0 = self.Parameters['m0'].copy(deepcopy=True)
+            self.isFunction(self.m0)
+        else:   self.m0 = Function(self.Vm)
         self.mtrial = TrialFunction(self.Vm)
         self.mtest = TestFunction(self.Vm)
         self.R = assemble(inner(nabla_grad(self.mtrial), \
         nabla_grad(self.mtest))*dx)
+        self.M = assemble(inner(self.mtrial, self.mtest)*dx)
        
     def get_R(self):
-        return self.gamma * self.R
+        return self.gamma*self.R + self.beta*self.M
+
+    def get_precond(self):
+        if self.beta > 1e-16: return self.get_R()
+        else:   return self.gamma*self.R + min(1e-14, self.gamma/10.)*self.M
 
     def cost(self, m_in):
         self.isFunction(m_in)
-        return 0.5*self.gamma * np.dot(m_in.vector().array(), \
-        (self.R*m_in.vector()).array())
+        diff = m_in.vector() - self.m0.vector()
+        return 0.5*( self.gamma*np.dot(diff.array(), (self.R*diff).array()) + \
+        self.beta*np.dot(diff.array(), (self.M*diff).array()) )
 
     def grad(self, m_in):
         self.isFunction(m_in)
-        return self.gamma * (self.R * m_in.vector())
+        diff = m_in.vector() - self.m0.vector()
+        return self.gamma*(self.R*diff) + self.beta*(self.M*diff)
 
     def hessian(self, m_in):
         self.isVector(m_in)
-        return self.gamma * (self.R * m_in)
+        return self.gamma*(self.R*m_in) + self.beta*(self.M*m_in)
