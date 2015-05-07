956355e97a18fe5c85e78aecc346b544b434dc18
Benjamin Crestel <crestel@ices.utexas.edu>
2015-05-07 15:49:50 -0500

    A few comments in exple

--- examples/poisson_Dirichlet.py
+++ examples/poisson_Dirichlet.py
@@ -1,3 +1,12 @@
+"""
+Medium parameter reconstruction example for Poisson pb with zero-Dirichlet
+boundary conditions.
+We solve
+arg min_m 1/2||u - u_e||^2 + R(m)
+where - Delta u = f, on Omega
+with u = 0 on boundary.
+"""
+
 import numpy as np
 from dolfin import *
 from fenicstools.objectivefunctional import ObjFctalElliptic
@@ -9,32 +18,34 @@ from fenicstools.miscfenics import apply_noise
 
 # Domain, f-e spaces and boundary conditions:
 mesh = UnitSquareMesh(20,20)
-V = FunctionSpace(mesh, 'Lagrange', 2)
-Vm = FunctionSpace(mesh, 'Lagrange', 1)
-Vme = FunctionSpace(mesh, 'Lagrange', 5)
+V = FunctionSpace(mesh, 'Lagrange', 2)  # space for state and adjoint variables
+Vm = FunctionSpace(mesh, 'Lagrange', 1) # space for medium parameter
+Vme = FunctionSpace(mesh, 'Lagrange', 5)    # sp for target med param
+# Define zero Boundary conditions:
 def u0_boundary(x, on_boundary):
     return on_boundary
 u0 = Constant("0.0")
 bc = DirichletBC(V, u0, u0_boundary)
-
-# Compute target data:
+# Define target medium and rhs:
 mtrue_exp = Expression('1 + 7*(pow(pow(x[0] - 0.5,2) +' + \
 ' pow(x[1] - 0.5,2),0.5) > 0.2)')
 mtrue = interpolate(mtrue_exp, Vme)
 normmtrue = norm(mtrue)
 f = Expression("1.0")
+
+# Compute target data:
 ObsOp = ObsEntireDomain({'V': V})
 goal = ObjFctalElliptic(V, Vme, bc, bc, [f], ObsOp, [], [], [], False)
 goal.update_m(mtrue)
 goal.solvefwd()
 UD = goal.U
-# Add noise
+# Add noise:
 noisepercent = 0.00   # e.g., 0.02 = 2% noise level
 UDnoise, objnoise = apply_noise(UD, noisepercent)
 print 'Noise in data misfit={:.5e}'.format(objnoise*.5/len(UD))
 
-# Set up optimization:
-Regul = TikhonovH1({'Vm': Vm, 'gamma': 1e-9})
+# Solve reconstruction problem:
+Regul = TikhonovH1({'Vm':Vm,'gamma':1e-9,'beta':1e-14})
 InvPb = ObjFctalElliptic(V, Vm, bc, bc, [f], ObsOp, UDnoise, Regul)
 InvPb.update_m(1.0) # Set initial medium
 InvPb.solvefwd_cost()
@@ -46,17 +57,18 @@ medmisfit = errornorm(InvPb.m, mtrue, 'l2', 1)
 print ('{:2d} {:12.5e} {:12.5e} {:12.5e} {:10.2e} {:6.3f}').format(0, \
 cost, misfit, regul, medmisfit, medmisfit/normmtrue)
 maxiter = 100 
-#meth = 'sd'
-meth = 'Newt'
+# Choose between steepest descent and Newton's method:
+METHODS = ['sd','Newt']
+meth = METHODS[1]
 if meth == 'sd':    alpha_init = 1e3
 elif meth == 'Newt':    alpha_init = 1.0
-nbcheck = 4
-nbLS = 20
-
-# Iteration
+nbcheck = 4 # Grad and Hessian checks
+nbLS = 20   # Max nb of line searches
+# Start iteration:
 for it in range(1, maxiter+1):
     InvPb.solveadj_constructgrad()
-    InvPb.mult(InvPb.Grad.vector(), InvPb.delta_m.vector())
+    #InvPb.mult(InvPb.Grad.vector(), InvPb.delta_m.vector())
+    # Check gradient and Hessian:
     if it == 1 or it % 10 == 0: 
         checkgradfd(InvPb, nbcheck)
         checkhessfd(InvPb, nbcheck)
@@ -64,10 +76,10 @@ for it in range(1, maxiter+1):
     InvPb.getMGarray()))
     if it == 1:   gradnorm_init = gradnorm
     gradnormrel = gradnorm/max(1.0, gradnorm_init)
-    tolcg = min(0.5, np.sqrt(gradnormrel))
+    tolcg = min(0.5, np.sqrt(gradnormrel))  # Inexact-CG-Newton's method
     compute_searchdirection(InvPb, meth, tolcg)
     LSsuccess, LScount, alpha = bcktrcklinesearch(InvPb, nbLS, alpha_init)
-    InvPb.plotm(it)
+    InvPb.plotm(it) # Plot current medium reconstruction
     # Print results
     srchdirnorm = np.sqrt(np.dot(InvPb.getsrchdirarray(), \
     (InvPb.MM*InvPb.getsrchdirarray())))
