b314f688194e7f810e623fab6ea8602e2090a8ae
Benjamin Crestel <crestel@ices.utexas.edu>
2015-09-28 13:49:16 -0500

    minor modif to test

--- Test/test_dft.py
+++ Test/test_dft.py
@@ -28,7 +28,30 @@ ax.plot(np.fft.fftshift(ffxi), np.fft.fftshift(ffn))
 xx = np.linspace(-5,5,100)
 fsinc = np.sinc(xx)
 ax.plot(xx, fsinc**2)
-ax.set_xlim(-5,5)
+ax.set_xlim(-5., 5.)
+
+
+"""
+We look at the Fourier transform of the function sine. We don't compute Fourier
+series so we can't expect to get a single frequency but if we look at sine over
+several periods, we should get one dominant frequency. We check we recover
+that with the DFT.
+"""
+
+tt = np.linspace(0., 10., 1000)
+f = 2.0
+bb = np.sin(2.*np.pi*f*tt)
+ff = np.fft.fft(bb)
+ffn = ff.real**2 + ff.imag**2
+ffxi = np.fft.fftfreq(len(tt), d = tt[1]-tt[0])
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.plot(np.fft.fftshift(ffxi), np.fft.fftshift(ffn))
+ax2.plot([f,f],[0.,ffn.max()],'k--')
+ax2.plot([-f,-f],[0.,ffn.max()],'k--')
+ax2.set_xlim(-5., 5.)
+fig2.suptitle('f = {} Hz'.format(f))
+
 
 plt.show(block=True)
 
