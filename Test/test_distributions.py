f81cd891418423c44b9a4ea1f556b0dcfb3bd21b
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2015-11-03 09:35:34 -0600

    update and rename test distr

--- Test/test_distributions.py
+++ Test/test_distributions.py
@@ -7,18 +7,15 @@ D = np.random.randn(dim_m**2).reshape((dim_m, dim_m))
 U,S,VT = np.linalg.svd(D)
 C = U.dot(np.diag(S**2)).dot(U.T)
 C_cholesky = U.dot(np.diag(S))
-assert np.linalg.norm(C - C_cholesky.dot(C_cholesky.T))/np.linalg.norm(C) < 1e-16
+checkvalue = np.linalg.norm(C - C_cholesky.dot(C_cholesky.T))/np.linalg.norm(C)
+assert checkvalue < 2e-16, checkvalue
 
 sample_size = 100000
-m_emp = np.zeros((dim_m, 1))
-C_emp = np.zeros((dim_m, dim_m))
-for ii in range(sample_size):
-    sample = SampleMultivariateNormal(m, C_cholesky).reshape((dim_m, 1))
-    m_emp += sample
-    C_emp += (sample-m).dot((sample-m).T)
+samples = SampleMultivariateNormal(m, C_cholesky, sample_size)
+m_emp = samples.sum(axis=1).reshape((dim_m,1))/sample_size
+m_one = m.dot(np.ones((1,sample_size)))
+C_emp = ((samples-m_one).dot((samples-m_one).T))/sample_size
 
-m_emp /= (sample_size-1)
-C_emp /= (sample_size-1)
 print "m, m_emp, m-m_emp: "
 print np.concatenate((m, m_emp, m-m_emp), axis=1)
 print np.sqrt(np.sum((m_emp-m)**2))/np.sqrt(np.sum(m**2))
