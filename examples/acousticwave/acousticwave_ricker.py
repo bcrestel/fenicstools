042c056e2abd39a120c1c5545a3d36a36339870e
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2015-12-10 14:01:12 -0600

    convergence exple with Ricker src

--- examples/acousticwave/acousticwave_ricker.py
+++ examples/acousticwave/acousticwave_ricker.py
@@ -16,41 +16,47 @@ except:
     mycomm = None
     myrank = 0
 
+RR = np.array((1, 2, 3, 4))
+ERROR = []
+
 # Inputs:
 Nxy = 100
 mesh = UnitSquareMesh(Nxy, Nxy, "crossed")
 h = 1./Nxy
-r = 3   # Polynomial order for solution
-V = FunctionSpace(mesh, 'Lagrange', r)
 Vl = FunctionSpace(mesh, 'Lagrange', 1)
-Dt = 4e-4   #Dt = h/(r*alpha)
-tf = 0.65
+Dt = 3e-4   #Dt = h/(r*alpha)
+tf = 1.5
 
 # Source term:
-Pt = PointSources(V, [[.5,.5]])
-mydelta = Pt[0].array()
 fpeak = 4. # .4Hz => up to 10Hz in input signal
 Ricker = RickerWavelet(fpeak, 1e-10)
-def mysrc(tt):
-    return Ricker(tt)*mydelta
 
 # Boundary conditions:
 class AllFour(SubDomain):
     def inside(self, x, on_boundary):
         return on_boundary
 
-# Computation:
-if myrank == 0: print '\n\th = {}, Dt = {}'.format(h, Dt)
-Wave = AcousticWave({'V':V, 'Vl':Vl, 'Vr':Vl})
-#Wave.verbose = True
-Wave.timestepper = 'centered'
-Wave.lump = True
-Wave.set_abc(mesh, AllFour(), True)
-Wave.update({'lambda':1.0, 'rho':1.0, 't0':0.0, 'tf':tf, 'Dt':Dt,\
-'u0init':Function(V), 'utinit':Function(V)})
-Wave.ftime = mysrc
-sol, error = Wave.solve()
-if not mycomm == None:  MPI.barrier(mycomm)
+for r in RR:
+    V = FunctionSpace(mesh, 'Lagrange', r)
+    Pt = PointSources(V, [[.5,.5]])
+    mydelta = Pt[0].array()
+    def mysrc(tt):
+        return Ricker(tt)*mydelta
+    # Computation:
+    if myrank == 0: print '\n\th = {}, Dt = {}'.format(h, Dt)
+    Wave = AcousticWave({'V':V, 'Vl':Vl, 'Vr':Vl})
+    #Wave.verbose = True
+    Wave.timestepper = 'centered'
+    Wave.lump = True
+    Wave.set_abc(mesh, AllFour(), True)
+    Wave.exact = Function(V)
+    Wave.update({'lambda':1.0, 'rho':1.0, 't0':0.0, 'tf':tf, 'Dt':Dt,\
+    'u0init':Function(V), 'utinit':Function(V)})
+    Wave.ftime = mysrc
+    sol, error = Wave.solve()
+    ERROR.append(error)
+    if myrank == 0: print 'relative error = {:.5e}'.format(error)
+    if not mycomm == None:  MPI.barrier(mycomm)
 
 # Plots:
 try:
