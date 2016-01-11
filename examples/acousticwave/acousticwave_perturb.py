46fac60a272d1caec3bb83a25aa7d2b78bc7175a
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-01-11 12:22:27 -0600

    add medium perturbation to exple

--- examples/acousticwave/acousticwave_perturb.py
+++ examples/acousticwave/acousticwave_perturb.py
@@ -28,7 +28,7 @@ mesh = UnitSquareMesh(Nxy, Nxy, "crossed")
 h = 1./Nxy
 Vl = FunctionSpace(mesh, 'Lagrange', 1)
 r = 2
-Dt = 2e-3   #Dt = h/(r*alpha)
+Dt = 2e-3   #Dt = h/(r*alpha*c_max)
 tf = 6.0
 
 # Source term:
@@ -52,12 +52,20 @@ Wave.verbose = True
 Wave.timestepper = 'backward'
 Wave.lump = True
 #Wave.set_abc(mesh, AllFour(), True)
-#TODO: need to define lambda medium with perturbation in the middle
-Wave.update({'lambda':1.0, 'rho':1.0, 't0':0.0, 'tf':tf, 'Dt':Dt,\
-'u0init':Function(V), 'utinit':Function(V)})
+lambda_target = Expression('1.0 + 0.0*(' \
+'(x[0]>=0.3)*(x[0]<=0.7)*(x[1]>=0.3)*(x[1]<=0.7) +' \
+'((x[0]-0.2)*10*(x[0]>0.2)*(x[0]<0.3) +' \
+'(-x[0]+0.8)*10*(x[0]>0.7)*(x[0]<0.8))*(x[1]>0.2)*(x[1]<0.8) +' \
+'((x[1]-0.2)*10*(x[1]>0.2)*(x[1]<0.3) +' \
+'(-x[1]+0.8)*10*(x[1]>0.7)*(x[1]<0.8))*(x[0]>0.2)*(x[0]<0.8))')    # square perturbation in the middle
+lambda_target_fn = interpolate(lambda_target, Vl)
+Wave.update({'lambda':lambda_target_fn, 'rho':1.0, \
+'t0':0.0, 'tf':tf, 'Dt':Dt, 'u0init':Function(V), 'utinit':Function(V)})
 Wave.ftime = mysrc
 sol, tmp = Wave.solve()
 if not mycomm == None:  MPI.barrier(mycomm)
+#TODO: Add observation and check with paraview
+#TODO: Add observation along with fading filter
 
 # Plots:
 try:
@@ -66,6 +74,7 @@ except:
     boolplot = 50
 if boolplot > 0:
     filename, ext = splitext(sys.argv[0])
+    filename = filename + '0'
     if myrank == 0: 
         if isdir(filename + '/'):   rmtree(filename + '/')
     if not mycomm == None:  MPI.barrier(mycomm)
@@ -77,3 +86,9 @@ if boolplot > 0:
             plotp.vector()[:] = pp[0]
             myplot.plot_vtk(plotp, index)
     myplot.gather_vtkplots()
+    # Plot medium
+    myplot.set_varname('lambda')
+    myplot.plot_vtk(lambda_target_fn)
+#TODO: Write python script to take different between homogeneous results (in
+#/ac..._perturb0/) and perturbed medium (in /ac.._perturb/).
+# Solutions, in vtu files are stored in the last section DataArray.
