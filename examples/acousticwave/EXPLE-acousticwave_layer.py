6b9b77844ec18d0bbee18d3aeb40746323601ef4
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-01-14 09:26:01 -0600

    create new layer example

--- examples/acousticwave/EXPLE-acousticwave_layer.py
+++ examples/acousticwave/EXPLE-acousticwave_layer.py
@@ -1,15 +1,14 @@
-"""
-Implement simple layer model with free surface on the top and abs on the other 3
-sides, Ricker wave on the surface
-"""
+""" 1D (quadratic) wave hitting a layer + abs bndy conditions """
 
 import sys
 from os.path import splitext, isdir
 from shutil import rmtree
+import numpy as np
+import matplotlib.pyplot as plt
 
 from fenicstools.plotfenics import PlotFenics
 from fenicstools.acousticwave import AcousticWave
-from fenicstools.sourceterms import PointSources, RickerWavelet
+from fenicstools.miscfenics import checkdt_abc
 try:
     from dolfin import UnitSquareMesh, FunctionSpace, Constant, DirichletBC, \
     interpolate, Expression, Function, SubDomain, MPI, mpi_comm_world
@@ -21,51 +20,47 @@ except:
     mycomm = None
     myrank = 0
 
-rr = 3  # Polynomial degree
-
-# Inputs:
-Nxy = 100
-Dx, Dy = 1.0, 2.0
-mesh = RectangleMesh(0., 0., Dx, Dy, int(Dx*Nxy), int(Dx*Nxy), "crossed")
-h = 1./Nxy
-Vl = FunctionSpace(mesh, 'Lagrange', 1)
-Dt = 4e-4   #Dt = h/(r*alpha)
-tf = 1.5
-
-# Source term:
-fpeak = 4. # .4Hz => up to 10Hz in input signal
-Ricker = RickerWavelet(fpeak, 1e-10)
-
-# Boundary conditions:
-class FreeSurfonTop(SubDomain):
+direction = 0   # direction of the 1D wave
+u0_expr = Expression(\
+'100*pow(x[i]-.5,2)*pow(x[i]-1.,2)*(x[i]<=1.)*(x[i]>=0.5)', i=direction)
+class LeftRight(SubDomain):
     def inside(self, x, on_boundary):
-        return x[1] < Dy and on_boundary
+        return (x[direction] < 1e-16 or x[direction] > 1.0 - 1e-16) \
+        and on_boundary
 
-V = FunctionSpace(mesh, 'Lagrange', r)
-Pt = PointSources(V, [[.5*Dx, Dy]]) # On the top surface
-mydelta = Pt[0].array()
-def mysrc(tt):
-    return Ricker(tt)*mydelta
-# Computation:
+q = 3
+if myrank == 0: print '\npolynomial order = {}'.format(q)
+alpha = 3.
+
+Nxy = 40
+h = 1./Nxy
+mesh = UnitSquareMesh(Nxy, Nxy)
+V = FunctionSpace(mesh, 'Lagrange', q)
+Vl = FunctionSpace(mesh, 'Lagrange', 1)
+c_max = 2
+#Dt = h/(q*alpha*c_max)
+Dt = 1e-3
+checkdt_abc(Dt, h, q, c_max, True, True, 'centered')
 if myrank == 0: print '\n\th = {}, Dt = {}'.format(h, Dt)
+
 Wave = AcousticWave({'V':V, 'Vl':Vl, 'Vr':Vl})
 #Wave.verbose = True
 Wave.timestepper = 'centered'
 Wave.lump = True
-Wave.set_abc(mesh, FreeSurfonTop(), True)
-#TODO: need to modify medium properties
-Wave.update({'lambda':1.0, 'rho':1.0, 't0':0.0, 'tf':tf, 'Dt':Dt,\
-'u0init':Function(V), 'utinit':Function(V)})
-Wave.ftime = mysrc
+Wave.set_abc(mesh, LeftRight(), True)
+# Medium ppties:
+lam_expr = Expression('1.0 + 3.0*(x[i]<=0.25)', i=direction)
+lam = interpolate(lam_expr, Vl)
+Wave.update({'lambda':lam, 'rho':1.0, 't0':0.0, 'tf':1.5, 'Dt':Dt,\
+'u0init':interpolate(u0_expr, V), 'utinit':Function(V)})
 sol, tmp = Wave.solve()
-if myrank == 0: print 'relative error = {:.5e}'.format(error)
 if not mycomm == None:  MPI.barrier(mycomm)
 
-# Plots:
+# Save plots:
 try:
     boolplot = int(sys.argv[1])
 except:
-    boolplot = 0
+    boolplot = 10
 if boolplot > 0:
     filename, ext = splitext(sys.argv[0])
     if myrank == 0: 
@@ -79,3 +74,7 @@ if boolplot > 0:
             plotp.vector()[:] = pp[0]
             myplot.plot_vtk(plotp, index)
     myplot.gather_vtkplots()
+    # Plot medium
+    myplot.set_varname('lambda')
+    myplot.plot_vtk(lam)
+
