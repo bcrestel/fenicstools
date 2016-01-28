4da760d290b1eadd4f1b28284b5f361da63e3941
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-01-28 11:40:23 -0600

    test TV regularization -- weird behavior for order > 1

---
+++ Test/test_TV.py
@@ -0,0 +1,94 @@
+"""
+Test for the TV regularization class.
+- All tests passed except along the most sinuating direction (last one)
+- tested with k=1.0 and k=exp
+- test with eps=0.0001, 0.001, 0.01 and eps=0.1.
+- test with V order 1. !!!! DOES NOT CHECK WITH ORDER > 1 !!!!
+"""
+
+import dolfin as dl
+import numpy as np
+
+from fenicstools.regularization import TV
+from fenicstools.miscfenics import setfct
+
+HH = [1e-4, 1e-5, 1e-6]
+
+mesh = dl.UnitSquareMesh(100, 100)
+V = dl.FunctionSpace(mesh, 'Lagrange', 1)
+k_exp = dl.Expression('1.0 + exp(x[0]*x[1])')
+k = dl.interpolate(k_exp, V)
+#k = dl.Constant(1.0)
+m_in = dl.Function(V)
+TV1 = TV({'Vm':V, 'eps':dl.Constant(0.0001), 'k':k})
+
+print 'Test 1: Smooth medium'
+
+# Verification point, i.e., point at which gradient and Hessian are checked
+m_exp = dl.Expression('sin(n*pi*x[0])*sin(n*pi*x[1])', n=4)
+m = dl.interpolate(m_exp, V)
+
+print '\nGradient:'
+failures = 0
+for nn in range(8):
+    print '\ttest ' + str(nn+1)
+    dm_exp = dl.Expression('sin(n*pi*x[0])*sin(n*pi*x[1])', n=nn+1)
+    dm = dl.interpolate(dm_exp, V)
+
+    for h in HH:
+        success = False
+        setfct(m_in, m)
+        m_in.vector().axpy(h, dm.vector())
+        cost1 = TV1.cost(m_in)
+
+        setfct(m_in, m)
+        m_in.vector().axpy(-h, dm.vector())
+        cost2 = TV1.cost(m_in)
+
+        GradFD = (cost1 - cost2)/(2.*h)
+
+        Gradm = TV1.grad(m) 
+        Gradm_h = Gradm.inner(dm.vector())
+
+        err = np.abs(GradFD-Gradm_h)/np.abs(Gradm_h)
+        print 'h={}, GradFD={}, Gradm_h={}, err={}'.format(\
+        h, GradFD, Gradm_h, err)
+        if err < 1e-6:  
+            print 'test {}: OK!'.format(nn+1)
+            success = True
+            break
+    if not success: failures+=1
+print '\nTest gradient -- Summary: {} test(s) failed'.format(failures)
+
+if failures < 5:
+    print '\n\nHessian:'
+    failures = 0
+    for nn in range(8):
+        print '\ttest ' + str(nn+1)
+        dm_exp = dl.Expression('sin(n*pi*x[0])*sin(n*pi*x[1])', n=nn+1)
+        dm = dl.interpolate(dm_exp, V)
+
+        for h in HH:
+            success = False
+            setfct(m_in, m)
+            m_in.vector().axpy(h, dm.vector())
+            grad1 = TV1.grad(m_in)
+            #
+            setfct(m_in, m)
+            m_in.vector().axpy(-h, dm.vector())
+            grad2 = TV1.grad(m_in)
+            #
+            HessFD = (grad1 - grad2)/(2.*h)
+
+            TV1.assemble_hessian(m)
+            Hessmdm = TV1.hessian(dm.vector())
+
+            err = (HessFD-Hessmdm).norm('l2')/Hessmdm.norm('l2')
+            print 'h={}, err={}'.format(h, err)
+
+            if err < 1e-6:  
+                print 'test {}: OK!'.format(nn+1)
+                success = True
+                break
+        if not success: failures+=1
+    print '\nTest Hessian --  Summary: {} test(s) failed\n'.format(failures)
--- regularization.py
+++ regularization.py
@@ -1,12 +1,15 @@
+import sys
 
-from dolfin import sqrt, inner, nabla_grad, dx, \
+from dolfin import sqrt, inner, nabla_grad, grad, dx, \
 Function, TestFunction, TrialFunction, assemble
 from miscfenics import isFunction, isVector, setfct
 
-#TODO: to be checked
+#TODO: investigate cause of gradient inacuraccy
 class TV():
     """
     Define Total Variation regularization
+    !!!! WARNING: SHOULD USE CONTINUOUS LINEAR ELEMENTS !!!!
+    Gradient does not check with higher-order elements
     """
 
     def __init__(self, parameters):
@@ -16,24 +19,55 @@ class TV():
             - eps = regularization parameter
         ||f||_TV = int k(x) sqrt{|grad f|^2 + eps} dx
         """
-        self.k = parameters['k']
-        self.eps = parameters['eps']
-        self.Vm = parameters['Vm']
-        self.m = Function(self.Vm)
-        self.test = TestFunction(self.Vm)
-        self.trial = TrialFunction(self.Vm)
-        #
-        self.wkformcost = self.k * \
-        sqrt(inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)*dx
-        #
-        self.wkformgrad = inner(nabla_grad(self.m), nabla_grad(self.test)) * \
-        self.k/sqrt(inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)*dx
-        #
-        self.wkformhess = (inner(nabla_grad(self.test),nabla_grad(self.trial)) -\
-        inner(nabla_grad(self.test),nabla_grad(self.m))*\
-        inner(nabla_grad(self.m),nabla_grad(self.trial))/\
-        (inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)) * \
-        self.k/sqrt(inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)*dx
+        if parameters.has_key('eps') and parameters.has_key('Vm'):
+            if not parameters.has_key('k'):
+                parameters['k'] = 1.0
+            self.update(parameters)
+        else:   
+            print "inputs parameters must contain field 'eps' and 'Vm'"
+            sys.exit(1)
+
+    def update(self, parameters):
+        """ Update the parameters.
+        parameters should be:
+            - k(x) = factor inside TV
+            - eps = regularization parameter
+            - Vm = FunctionSpace for parameter. !!!! WARNING: SHOULD BE ORDER 1 !!!!
+        ||f||_TV = int k(x) sqrt{|grad f|^2 + eps} dx
+        """
+        if parameters.has_key('k'): self.k = parameters['k']
+        if parameters.has_key('eps'): self.eps = parameters['eps']
+        if parameters.has_key('Vm'):
+            self.Vm = parameters['Vm']
+            self.m = Function(self.Vm)
+            self.test = TestFunction(self.Vm)
+            self.trial = TrialFunction(self.Vm)
+            self.H = None
+
+            self.fTV = inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps
+            self.kovsq = self.k / sqrt(self.fTV)
+
+            self.wkformcost = self.k * sqrt(inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)*dx
+            #
+            self.wkformgrad = self.kovsq*inner(nabla_grad(self.m), nabla_grad(self.test))*dx
+            #
+            self.wkformhess = self.kovsq * ( \
+            inner(nabla_grad(self.trial), nabla_grad(self.test)) - \
+            inner(nabla_grad(self.m), nabla_grad(self.test))* \
+            inner(nabla_grad(self.trial), nabla_grad(self.m))/self.fTV)*dx
+#
+#            self.wkformcost = self.k * \
+#            sqrt(inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)*dx
+#            #
+#            self.wkformgrad = inner(nabla_grad(self.m), nabla_grad(self.test)) * \
+#            self.k/sqrt(inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)*dx
+#            #
+#            self.wkformhess = (inner(nabla_grad(self.trial),nabla_grad(self.test)) -\
+#            inner(nabla_grad(self.m),nabla_grad(self.test))*\
+#            inner(nabla_grad(self.trial),nabla_grad(self.m))/\
+#            (inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)) * \
+#            self.k/sqrt(inner(nabla_grad(self.m), nabla_grad(self.m)) + self.eps)*dx
+
 
 
     def cost(self, m_in):
@@ -48,9 +82,9 @@ class TV():
         setfct(self.m, m_in)
         return assemble(self.wkformgrad)
 
-    def assemble_hessian(self, m_in=None):
+    def assemble_hessian(self, m_in):
         """ Assemble the Hessian of TV at m_in """
-        if not m_in == None:    setfct(self.m, m_in)
+        setfct(self.m, m_in)
         self.H = assemble(self.wkformhess)
 
     def hessian(self, mhat):
