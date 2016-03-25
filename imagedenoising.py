8238feaeae54c0d05f44dfb4be27d0f83fe9830c
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-03-25 17:15:44 -0500

    add TV-based image denoising

--- imagedenoising.py
+++ imagedenoising.py
@@ -6,6 +6,7 @@ import dolfin as dl
 
 from miscfenics import setfct
 from prior import LaplacianPrior
+from regularization import TV
 
 class ObjectiveImageDenoising1D():
     """
@@ -43,6 +44,8 @@ class ObjectiveImageDenoising1D():
         if self.regularization == 'tikhonov':
             self.RegTikh = LaplacianPrior({'gamma':1.0, 'beta':0.0, 'Vm':self.V})
             self.R = self.RegTikh.Minvprior.array()
+        elif self.regularization == 'TV':
+            self.RegTV = TV({'eps':1e-2, 'Vm':self.V})
 
     def assembleK(self):
         self.K = np.zeros((self.dimV, self.dimV))
@@ -55,13 +58,15 @@ class ObjectiveImageDenoising1D():
         self.d = self.K.dot(self.f)
         sigma = noisepercent*np.linalg.norm(self.d)/np.sqrt(self.dimV)
         eta = sigma*np.random.randn(self.dimV)
-        print 'noise residual={}'.format(np.linalg.norm(eta)**2)
+        print 'noise residual={}'.format(.5*np.linalg.norm(eta)**2)
         self.dn = self.d + eta
 
-    def update_reg(self, parameters):
+    def update_reg(self, gamma):
         if self.regularization == 'tikhonov':
-            self.gamma = parameters
+            self.gamma = gamma
             self.R = self.RegTikh.Minvprior.array()*self.gamma
+        elif self.regularization == 'TV':
+            self.RegTV.update({'k':gamma})
 
 
     ### COST and DERIVATIVES
@@ -69,18 +74,82 @@ class ObjectiveImageDenoising1D():
         """ Compute cost functional at f """
         if f == None:   f = self.g
         self.misfit = .5*np.linalg.norm(self.K.dot(f) - self.dn)**2
-        self.reg = .5*(self.R.dot(f)).dot(f)
+        if self.regularization == 'tikhonov':   self.reg = .5*(self.R.dot(f)).dot(f)
+        elif self.regularization == 'TV':   self.reg = self.RegTV.cost(f)
         self.cost = self.misfit + self.reg
         return self.cost
 
-    def gradient(self, f):
+    def gradient(self, f=None):
         """ Compute M.g (discrete gradient) at a given point f """
-        self.MG = self.K.T.dot(self.K.dot(f) - self.dn) + self.R.dot(f)
+        if f == None:   f = self.g
+        self.MGk = self.K.T.dot(self.K.dot(f) - self.dn) 
+        if self.regularization == 'tikhonov':   self.MGr = self.R.dot(f)
+        elif self.regularization == 'TV':   self.MGr = self.RegTV.grad(f).array()
+        self.MG = self.MGk + self.MGr
 
-    def Hessian(self, f):
+    def Hessian(self, f=None):
         """ Assemble Hessian at f """
-        self.Hess = self.K.T.dot(self.K) + self.R
+        if f == None:   f = self.g
+        self.Hessk = self.K.T.dot(self.K) 
+        if self.regularization == 'tikhonov':   self.Hessr = self.R
+        elif self.regularization == 'TV':
+            self.RegTV.assemble_hessian(f)
+            self.Hessr = self.RegTV.H.array()
+        self.Hess = self.Hessk + self.Hessr
         
+        
+    ### SOLVER
+    def searchdirection(self):
+        """ Compute search direction """
+        self.gradient()
+        self.Hessian()
+        self.df = np.linalg.solve(self.Hess, -self.MG)
+        assert self.df.dot(self.MG) < 0.0, "not a descent direction"
+
+    def linesearch(self, alpha0=1.0, rho=0.5, c=5e-5):
+        """ Perform inexact backtracking line search """
+        costref = self.cost
+        cdJdf = c*self.MG.dot(self.df)
+        self.alpha = alpha0
+        self.LS = False
+        for ii in xrange(12):
+            if self.computecost(self.g + self.alpha*self.df) < \
+            costref + self.alpha*cdJdf:
+                self.g = self.g + self.alpha*self.df
+                self.LS = True
+                break
+            else:
+                self.alpha *= rho
+
+    def solve(self):
+        """ Solve image denoising pb """
+        if self.regularization == 'tikhonov':
+            self.Hessian(None)
+            self.g = np.linalg.solve(self.Hess, self.K.T.dot(self.dn))
+            self.computecost()
+            self.alpha = 1.0
+        elif self.regularization == 'TV':
+            self.computecost()
+            self.alpha = 1.0
+            self.printout()
+            cost = self.cost
+            for ii in xrange(500):
+                self.searchdirection()
+                self.linesearch(0.1)
+                print ii,
+                self.printout()
+                if ii%50 == 0:
+                    self.plot()
+                    plt.show()
+                if not self.LS:
+                    print 'Line search failed'
+                    break
+                if np.abs(cost - self.cost)/cost < 1e-5:
+                    print 'optimization converged'
+                    break
+                cost = self.cost
+
+
     ### TESTS
     def test_gradient(self, f=None, n=5):
         """ test gradient with FD approx around point f """
@@ -117,34 +186,6 @@ class ObjectiveImageDenoising1D():
             print 'n={}:\tHFD={:.5e}, Hdf={:.5e}, error={:.2e}'.format(\
             nn, np.linalg.norm(HFD), np.linalg.norm(Hdf), \
             np.linalg.norm(Hdf-HFD)/np.linalg.norm(Hdf))
-        
-    ### SOLVER
-    def searchdirection(self):
-        """ Compute search direction """
-        self.gradient(self.g)
-        self.Hessian(self.g)
-        self.df = np.linalg.solve(self.Hess, -self.MG)
-        assert self.df.dot(self.MG) < 0.0, "not a descent direction"
-        print self.df.dot(self.MG)
-
-    def linesearch(self):
-        """ Perform exact line search """
-        def costLS(alpha):  return self.computecost(self.g + alpha*self.df)
-        self.alpha = optimize.fmin(costLS, 1.0)
-#        costLS = np.vectorize(costLS)
-#        alpha = np.linspace(0.0, 5.0, 1000)
-#        fig2 = plt.figure()
-#        ax = fig2.add_subplot(111)
-#        ax.plot(alpha, costLS(alpha))
-        self.g = self.g + self.alpha*self.df
-
-    def solve(self):
-        """ Solve image denoising pb """
-        if self.regularization == 'tikhonov':
-            self.Hessian(None)
-            self.g = np.linalg.solve(self.Hess, self.K.T.dot(self.dn))
-            self.computecost()
-            self.alpha = 1.0
 
 
     ### OUTPUT
