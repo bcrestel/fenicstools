d314a2fd015f6562b567f784afd6752359ccccad
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-03-25 17:15:50 -0500

    minor

--- examples/image-denoising/fenics-1dTikhonov.py
+++ examples/image-denoising/fenics-1dTikhonov.py
@@ -18,21 +18,27 @@ CC = 1/(gamma*np.sqrt(2*np.pi))
 k_e = dl.Expression('C*exp(-pow(x[0]-t,2)/(2*pow(g,2)))', t=0., C=CC, g=gamma)
 denoise = ObjectiveImageDenoising1D(mesh, k_e)
 denoise.generatedata(0.2)
+#g = .5*np.abs(np.sin(np.pi*denoise.xx*2))
 #denoise.test_gradient(g)
 #denoise.test_hessian(g)
+"""
 GAMMA = 10**(-np.linspace(0.0, 5.0, 100))
 RMM = []
 COST = []
 for gamma in GAMMA:
-    print 'gamma=', gamma,
+    print 'gamma={:.2e}'.format(gamma),
     denoise.update_reg(gamma)
     denoise.solve()
     denoise.printout()
     RMM.append(denoise.relmedmisfit)
     COST.append(denoise.cost)
-fig = plt.figure(); ax=fig.add_subplot(111); ax.semilogx(GAMMA, RMM)
-fig2 = plt.figure(); ax=fig2.add_subplot(111); ax.semilogx(GAMMA, COST)
+fig = plt.figure(); ax=fig.add_subplot(111); ax.semilogx(GAMMA, RMM);
+ax.set_label('gamma'); ax.set_ylabel('rel med misfit')
+fig2 = plt.figure(); ax=fig2.add_subplot(111); ax.semilogx(GAMMA, COST);
+ax.set_label('gamma'); ax.set_ylabel('objective (=misfit+reg)')
+"""
 denoise.update_reg(10**(-1.5))
 denoise.solve()
+denoise.printout()
 fig3 = denoise.plot()
 plt.show()
