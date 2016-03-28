0b5d1349b4a1504e79024d328d8eb7fb8aee6c98
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-03-28 10:03:49 -0500

    cycle skipping exple with deblurring kernel

--- examples/inverse-acousticwave/EXPLE-acousticinv-cycleskipping.py
+++ examples/inverse-acousticwave/EXPLE-acousticinv-cycleskipping.py
@@ -5,35 +5,61 @@ import dolfin as dl
 import numpy as np
 import matplotlib.pyplot as plt
 
-#TODO: Try with sinusoidal wave instead of Ricker wavelet -> more local minima
 
-times = np.linspace(0.,2.,1000)
-RW = RickerWavelet(3., 1e-10)
-mesh = dl.UnitSquareMesh(10,10)
-V = dl.FunctionSpace(mesh, 'CG', 1)
-obs = TimeObsPtwise({'V':V, 'Points':[]})
-Rref = RW(times-0.5)
-RR = []
-dt = []
-for ii in range(200):
-    dt.append(-(ii-100)/200.)
-    RR.append(RW(times-0.5-(ii-100)/200.))
+tt = np.linspace(-1., 16., 1000)
+dt = tt[1]-tt[0]
+k = np.array([[(i-j)*dt for i in range(1000)] for j in range(1000)])
+sigma = 2.0
+K = np.exp(-k**2/(2.*sigma**2))
 
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(111)
-ax1.plot(times, Rref, 'k')
-ax1.plot(times, RR[0], 'r--')
-ax1.plot(times, RR[-1], 'r--')
-fig1.savefig('cycleskipping1.pdf')
-dm = []
-dm2 = []
-for rr in RR:
-    dm.append(obs.costfct(Rref, rr, times))
-    dm2.append(np.exp(-obs.costfct(Rref, rr, times)**2/2))
+# test case for K:
+time = np.linspace(0.0, 1.0, 1000)
+noisy = np.sin(2*np.pi*time) + 0.1*np.sin(20*np.pi*time)
+noisy = noisy/np.max(np.abs(noisy))
+Knoisy = K.dot(noisy)
+Knoisy = Knoisy/np.max(np.abs(Knoisy))
+figK = plt.figure()
+ax = figK.add_subplot(111)
+ax.plot(time, noisy, label='noisy')
+ax.plot(time, Knoisy, label='K*noisy')
+ax.legend()
+
+
+def misfit(tt, s1, s2):
+    dt = tt[1] - tt[0]
+    ds = s2 - s1
+    factors = np.ones(tt.shape)
+    factors[0], factors[-1] = 0.5, 0.5
+    return dt*(factors*(ds**2)).sum()
+
+def misfitK(tt, s1, s2):
+    dt = tt[1] - tt[0]
+    ds = s2 - s1
+    return dt*((K.dot(ds)).dot(ds))
+
+def shifted(tt, t0):
+    return (tt>=t0)*(tt<=t0+5.)*np.sin(2.*np.pi*(tt-t0))
+
+true = shifted(tt, 5.)
+t1 = shifted(tt, 2.5)
+
+MISFIT = []
+MISFITK = []
+for t0 in tt:
+    if t0 > 11.: break
+    signal = shifted(tt, t0)
+    MISFIT.append(misfit(tt, true, signal))
+    MISFITK.append(misfitK(tt, true, signal))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(tt, true)
+ax.plot(tt, t1)
+
 fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.plot(dt, dm, label='l2')
-ax2.plot(dt, dm2, label='exp')
-fig2.savefig('cycleskipping2.pdf')
-ax2.legend()
+ax = fig2.add_subplot(111)
+ax.plot(MISFIT)
+ax.plot(MISFITK)
+
 plt.show()
+
