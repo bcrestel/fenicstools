bbe7743f278a99453f5b6f3aa9e77748fde679cf
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-04-05 23:43:49 -0500

    continue denoising class

--- imaging.py
+++ imaging.py
@@ -1,4 +1,6 @@
 import sys
+import os.path
+import shutil
 import numpy as np
 from scipy import optimize
 import matplotlib.pyplot as plt
@@ -7,96 +9,108 @@ import dolfin as dl
 from miscfenics import setfct
 from prior import LaplacianPrior
 from regularization import TV
+from plotfenics import PlotFenics
 
 
 class ObjectiveImageDenoising():
     """
     Class to do image denoising
     """
-
-    def __init__(self, mesh, trueImage, regularization='tikhonov'):
+    def __init__(self, mesh, trueImage, parameters):
         """
         Inputs:
             mesh = Fenics mesh
+            trueImage = object from class Image
+            parameters = dict
         """
         # Mesh
         self.mesh = mesh
         self.V = dl.FunctionSpace(self.mesh, 'Lagrange', 1)
+        self.xx = self.V.dofmap().tabulate_all_coordinates(self.mesh)
         self.dimV = self.V.dim()
         self.test, self.trial = dl.TestFunction(self.V), dl.TrialFunction(self.V)
         self.f_true = dl.interpolate(trueImage, self.V)  
-        self.g = None   # current iterate
+        self.g, self.dg, self.gtmp = dl.Function(self.V), dl.Function(self.V), dl.Function(self.V)
+        self.Grad = dl.Function(self.V)
+        self.Gradnorm0 = None
         # mass matrix
         self.Mweak = dl.inner(self.test, self.trial)*dl.dx
         self.M = dl.assemble(self.Mweak)
+        self.solverM = dl.LUSolver('petsc')
+        self.solverM.parameters['symmetric'] = True
+        self.solverM.parameters['reuse_factorization'] = True
+        self.solverM.set_operator(self.M)
+        self.targetnorm = np.sqrt((self.M*self.f_true.vector()).inner(self.f_true.vector()))
         # regularization
-        self.parameters = {'regularization':regularization}
-        if regularization == 'tikhonov':
-            self.RegTikh = LaplacianPrior({'gamma':1.0, 'beta':0.0, 'Vm':self.V})
-            self.R = self.RegTikh.Minvprior.array()
-        elif regularization == 'TV':
-            self.RegTV = TV({'eps':1e-2, 'Vm':self.V})
+        self.parameters = parameters
+        self.define_regularization()
+        self.regparam = 1.0
         # line search parameters
         self.parameters['alpha0'] = 1.0
         self.parameters['rho'] = 0.5
         self.parameters['c'] = 5e-5
         self.parameters['max_backtrack'] = 12
+        # plots:
+        filename, ext = os.path.splitext(sys.argv[0])
+        if os.path.isdir(filename + '/'):   shutil.rmtree(filename + '/')
+        self.myplot = PlotFenics(filename)
+
         
     def generatedata(self, noisepercent):
         """ compute data and add noisepercent (%) of noise """
         sigma = noisepercent*np.linalg.norm(self.f_true.vector().array())/np.sqrt(self.dimV)
-        print sigma
+        print 'sigma_noise = ', sigma
         eta = sigma*np.random.randn(self.dimV)
         self.dn = dl.Function(self.V)
         setfct(self.dn, eta)
         self.dn.vector().axpy(1.0, self.f_true.vector())
 
-    def update_reg(self, gamma):
-        regularization = self.parameters['regularization']
+    def define_regularization(self, parameters=None):
+        if parameters == None:  parameters = self.parameters
+        regularization = parameters['regularization']
         if regularization == 'tikhonov':
-            self.gamma = gamma
-            self.R = self.RegTikh.Minvprior.array()*self.gamma
+            gamma = parameters['gamma']
+            beta = parameters['beta']
+            self.Reg = LaplacianPrior({'gamma':gamma, 'beta':beta, 'Vm':self.V})
+            self.inexact = False
         elif regularization == 'TV':
-            self.RegTV.update({'k':gamma})
+            eps = parameters['eps']
+            k = parameters['k']
+            GNhessian = parameters['GNhessian']
+            self.Reg = TV({'eps':eps, 'k':k, 'Vm':self.V, 'GNhessian':GNhessian})
+            self.inexact = True
 
 
     ### COST and DERIVATIVES
     def computecost(self, f=None):
         """ Compute cost functional at f """
-        regularization = self.parameters['regularization']
-        pbtype = self.parameters['pbtype']
         if f == None:   f = self.g
-        #
-        if pbtype == 'deblurring':  
-            self.misfit = .5*np.linalg.norm(self.K.dot(f)-self.dn)**2
-        elif pbtype == 'denoising':
-            self.misfit = .5*(self.M.dot(f-self.dn)).dot(f-self.dn)
-        if regularization == 'tikhonov':   self.reg = .5*(self.R.dot(f)).dot(f)
-        elif regularization == 'TV':   self.reg = self.RegTV.cost(f)
-        self.cost = self.misfit + self.reg
+        df = f.vector() - self.dn.vector()
+        self.misfit = 0.5 * (self.M*df).inner(df)
+        self.reg = self.Reg.cost(f)
+        self.cost = self.misfit + self.regparam*self.reg
         return self.cost
 
     def gradient(self, f=None):
         """ Compute M.g (discrete gradient) at a given point f """
-        regularization = self.parameters['regularization']
         if f == None:   f = self.g
-        #
-        self.MGk = self.K.T.dot(self.K.dot(f) - self.dn) 
-        if regularization == 'tikhonov':   self.MGr = self.R.dot(f)
-        elif regularization == 'TV':   self.MGr = self.RegTV.grad(f).array()
-        self.MG = self.MGk + self.MGr
+        df = f.vector() - self.dn.vector()
+        self.MGk = self.M*df
+        self.MGr = self.Reg.grad(f)
+        self.MG = self.MGk + self.MGr*self.regparam
+        self.solverM.solve(self.Grad.vector(), self.MG)
+        self.Gradnorm = np.sqrt((self.MG).inner(self.Grad.vector()))
+        if self.Gradnorm0 == None:  self.Gradnorm0 = self.Gradnorm
 
     def Hessian(self, f=None):
         """ Assemble Hessian at f """
-        regularization = self.parameters['regularization']
         if f == None:   f = self.g
-        #
-        self.Hessk = self.K.T.dot(self.K) 
-        if regularization == 'tikhonov':   self.Hessr = self.R
-        elif regularization == 'TV':
-            self.RegTV.assemble_hessian(f)
-            self.Hessr = self.RegTV.H.array()
-        self.Hess = self.Hessk + self.Hessr
+        regularization = self.parameters['regularization']
+        if regularization == 'TV':  
+            self.Reg.assemble_hessian(f)
+            self.Hess = self.M + self.Reg.H*self.regparam
+        elif regularization == 'tikhonov':
+            self.Hess = self.M + self.Reg.Minvprior*self.regparam
         
         
     ### SOLVER
@@ -104,117 +118,131 @@ class ObjectiveImageDenoising():
         """ Compute search direction """
         self.gradient()
         self.Hessian()
-        self.df = np.linalg.solve(self.Hess, -self.MG)
-        assert self.df.dot(self.MG) < 0.0, "not a descent direction"
+        solver = dl.PETScKrylovSolver("cg", "petsc_amg")
+        solver.parameters['nonzero_initial_guess'] = False
+        # Inexact CG:
+        if self.inexact:
+            self.cgtol = min(0.5, np.sqrt(self.Gradnorm/self.Gradnorm0))
+        else:   self.cgtol = 1e-6
+        solver.parameters['relative_tolerance'] = self.cgtol
+        solver.set_operator(self.Hess)
+        self.cgiter = solver.solve(self.dg.vector(), -1.0*self.MG)
+        if (self.MG).inner(self.dg.vector()) > 0.0:    
+            print "*** WARNING: NOT a descent direction"
 
     def linesearch(self):
         """ Perform inexact backtracking line search """
         self.alpha = self.parameters['alpha0']
         rho = self.parameters['rho']
         c = self.parameters['c']
+        self.computecost()
         costref = self.cost
-        cdJdf = c*self.MG.dot(self.df)
+        cdJdf = ( (self.MG).inner(self.dg.vector()) )*c
         self.LS = False
         for ii in xrange(self.parameters['max_backtrack']):
-            if self.computecost(self.g + self.alpha*self.df) < \
-            costref + self.alpha*cdJdf:
-                self.g = self.g + self.alpha*self.df
+            setfct(self.gtmp, self.g.vector() + self.dg.vector()*self.alpha) 
+            if self.computecost(self.gtmp) < costref + self.alpha*cdJdf:
+                self.g.vector().axpy(self.alpha, self.dg.vector())
                 self.LS = True
                 break
-            else:
-                self.alpha *= rho
+            else:   self.alpha *= rho
 
     def solve(self, plot=False):
         """ Solve image denoising pb """
         regularization = self.parameters['regularization']
+        print '\t{:12s} {:12s} {:12s} {:12s} {:12s} {:12s} {:12s}\t{:12s} {:12s}'.format(\
+        'a_reg', 'cost', 'misfit', 'reg', '||G||', 'a_LS', 'medmisfit', \
+        'tol_cg', 'n_cg')
         #
         if regularization == 'tikhonov':
-            self.Hessian(None)
-            self.g = np.linalg.solve(self.Hess, self.K.T.dot(self.dn))
-            self.computecost()
-            self.alpha = 1.0
-        elif regularization == 'TV':
+            self.searchdirection()
+            self.g.vector().axpy(1.0, self.dg.vector())
             self.computecost()
             self.alpha = 1.0
             self.printout()
+        elif regularization == 'TV':
+            self.computecost()
             cost = self.cost
-            self.COST = [cost]
-            self.MEDMIS = [self.relmedmisfit]
-            for ii in xrange(500):
+            for ii in xrange(1000):
                 self.searchdirection()
-                self.linesearch(1.0)
-                print ii,
+                self.linesearch()
+                print ii+1,
                 self.printout()
-                if plot and ii%50 == 0:
-                    self.plot()
-                    plt.show()
+                # Check termination conditions:
                 if not self.LS:
                     print 'Line search failed'
                     break
-                if np.abs(cost - self.cost)/cost < 1e-10:
+                if self.Gradnorm < min(1e-12, 1e-10*self.Gradnorm0) or \
+                np.abs(cost - self.cost)/cost < 1e-8:
                     print 'optimization converged'
                     break
                 cost = self.cost
-                self.COST.append(cost)
-                self.MEDMIS.append(self.relmedmisfit)
-
-
-    ### TESTS
-    def test_gradient(self, f=None, n=5):
-        """ test gradient with FD approx around point f """
-        if f == None:   f = self.f_true.copy()
-        pm = [1.0, -1.0]
-        eps = 1e-5
-        self.gradient(f)
-        for nn in xrange(1, n+1):
-            df = np.sin(np.pi*nn*self.xx)
-            cost = []
-            for sign in pm:
-                self.g = f + sign*eps*df
-                cost.append(self.computecost(self.g))
-            MGFD = (cost[0] - cost[1])/(2*eps)
-            MGdf = self.MG.dot(df)
-            print 'n={}:\tMGFD={:.5e}, MGdf={:.5e}, error={:.2e}'.format(\
-            nn, MGFD, MGdf, np.abs(MGdf-MGFD)/np.abs(MGdf))
-
-    def test_hessian(self, f=None, n=5):
-        """ test Hessian with FD approx around point f """
-        if f == None:   f = self.f_true.copy()
-        pm = [1.0, -1.0]
-        eps = 1e-5
-        self.Hessian(f)
-        for nn in xrange(1, n+1):
-            df = np.sin(np.pi*nn*self.xx)
-            MG = []
-            for sign in pm:
-                self.g = f + sign*eps*df
-                self.gradient(self.g)
-                MG.append(self.MG)
-            HFD = (MG[0] - MG[1])/(2*eps)
-            Hdf = self.Hess.dot(df)
-            print 'n={}:\tHFD={:.5e}, Hdf={:.5e}, error={:.2e}'.format(\
-            nn, np.linalg.norm(HFD), np.linalg.norm(Hdf), \
-            np.linalg.norm(Hdf-HFD)/np.linalg.norm(Hdf))
 
 
     ### OUTPUT
     def printout(self):
         """ Print results """
-        self.medmisfit = np.linalg.norm(self.g-self.f_true)
-        self.relmedmisfit = self.medmisfit/np.linalg.norm(self.f_true)
-        print 'cost={:.2e}, misfit={:.2e}, reg={:.2e}, alpha={:.2e}, medmisfit={:.2e} ({:.3f})'.format(\
-        self.cost, self.misfit, self.reg, self.alpha, self.medmisfit, self.relmedmisfit)
+        df = self.f_true.vector() - self.g.vector()
+        self.medmisfit = np.sqrt((self.M*df).inner(df))
+        self.relmedmisfit = self.medmisfit/self.targetnorm
+        print ('{:12.1e} {:12.4e} {:12.4e} {:12.4e} {:12.4e} {:12.2e} {:12.2e}'\
+        +' ({:.3f}) {:12.2e} {:6d}').format(\
+        self.regparam, self.cost, self.misfit, self.reg, self.Gradnorm, self.alpha, \
+        self.medmisfit**2, self.relmedmisfit, self.cgtol, self.cgiter)
 
-    def plot(self, index=0):
+    def plot(self, index=0, add=''):
         """ Plot target (w/ noise 0, or w/o noise 1) or current iterate (2) """
         if index == 0:
-            dl.plot(self.f_true)
-            dl.interactive()
+            self.myplot.set_varname('target' + add)
+            self.myplot.plot_vtk(self.f_true)
         elif index == 1:
-            dl.plot(self.dn)
-            dl.interactive()
-        #else:
+            self.myplot.set_varname('data' + add)
+            self.myplot.plot_vtk(self.dn)
+        elif index == 2:
+            self.myplot.set_varname('solution' + add)
+            self.myplot.plot_vtk(self.g)
 
+
+    ### TESTS
+    def test_gradient(self, f=None, n=5):
+        """ test gradient with FD approx around point f """
+        if f == None:   f = self.f_true
+        pm = [1.0, -1.0]
+        eps = 1e-5
+        self.gradient(f)
+        for nn in xrange(1, n+1):
+            expr = dl.Expression('sin(n*pi*x[0])*sin(n*pi*x[1])', n=nn)
+            df = dl.interpolate(expr, self.V)
+            MGdf = self.MG.inner(df.vector())
+            cost = []
+            for sign in pm:
+                setfct(self.g, f)
+                self.g.vector().axpy(sign*eps, df.vector())
+                cost.append(self.computecost(self.g))
+            MGFD = (cost[0] - cost[1])/(2*eps)
+            print 'n={}:\tMGFD={:.5e}, MGdf={:.5e}, error={:.2e}'.format(\
+            nn, MGFD, MGdf, np.abs(MGdf-MGFD)/np.abs(MGdf))
+
+    def test_hessian(self, f=None, n=5):
+        """ test Hessian with FD approx around point f """
+        if f == None:   f = self.f_true
+        pm = [1.0, -1.0]
+        eps = 1e-5
+        self.Hessian(f)
+        for nn in xrange(1, n+1):
+            expr = dl.Expression('sin(n*pi*x[0])*sin(n*pi*x[1])', n=nn)
+            df = dl.interpolate(expr, self.V)
+            Hdf = (self.Hess*df.vector()).array()
+            MG = []
+            for sign in pm:
+                setfct(self.g, f)
+                self.g.vector().axpy(sign*eps, df.vector())
+                self.gradient(self.g)
+                MG.append(self.MG.array())
+            HFD = (MG[0] - MG[1])/(2*eps)
+            print 'n={}:\tHFD={:.5e}, Hdf={:.5e}, error={:.2e}'.format(\
+            nn, np.linalg.norm(HFD), np.linalg.norm(Hdf), \
+            np.linalg.norm(Hdf-HFD)/np.linalg.norm(Hdf))
 
 
 
