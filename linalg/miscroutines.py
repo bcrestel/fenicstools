e2a5ff944f2ec15626f4e085c936ad833e75f287
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-08-26 14:48:04 -0500

    minor comment

--- linalg/miscroutines.py
+++ linalg/miscroutines.py
@@ -2,6 +2,6 @@ import numpy as np
 from dolfin import as_backend_type, PETScVector
 
 def get_diagonal(M):
-    """ Get diagonal of a square Matrix M and return a np.array"""
+    """ Extract diagonal of a square Matrix M and return a PETScVector """
 
     return PETScVector(as_backend_type(M).mat().getDiagonal())
