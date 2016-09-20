d0df44dce9ea38c694d639059fc21bf1f5264ce7
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-09-19 21:36:53 -0500

    separate computation MAP point from Hessian assembly

--- examples/assemble-Hessian/assemble-Hessian-b.py
+++ examples/assemble-Hessian/assemble-Hessian-b.py
@@ -31,20 +31,20 @@ mpicomm = mpi_comm_world()
 mpirank = MPI.rank(mpicomm)
 mpisize = MPI.size(mpicomm)
 
+# SAVE_MAP = True only computes the MAP point and print to file
+# SAVE_MAP = False loads the MAP point and compute the Hessian at that point
+SAVE_MAP = True
 PLOT = True
 
 # Input data:
-#Nxy = 100
-#Dt = 5e-4
-#T0TF = [[0.0, 0.02, 1.50, 1.52], [0.0, 0.04, 2.00, 2.02], [0.0, 0.5, 6.5, 7.0]]
-#FREQ = [10.0, 4.0, 0.5]
-#SKIP = [20, 20, 200]
-
-Nxy = 50
-Dt = 5e-4
-T0TF = [[0.0, 0.04, 2.00, 2.04], [0.0, 0.5, 6.5, 7.0]]
-FREQ = [4.0, 0.5]
-SKIP = [20, 100]
+Data={
+'4.0': [4.0, 50, 5e-4, [0.0, 0.04, 2.0, 2.04]], \
+'8.0': [8.0, 50, 5e-4, [0.0, 0.04, 2.0, 2.04]], \
+'14.0': [14.0, 140, 5e-4, [0.0, 0.02, 1.5, 1.52]]
+}
+freq, Nxy, Dt, t0tf = Data['4.0']
+t0, t1, t2, tf = t0tf
+skip = int(0.1/Dt)
 checkdt(Dt, 1./Nxy, 2, np.sqrt(2.0), True)
 
 # mesh
@@ -60,10 +60,19 @@ b_target_fn = dl.interpolate(b_target, Vm)
 a_target = dl.Expression('1.0')
 a_target_fn = dl.interpolate(a_target, Vm)
 
+# set up plots:
+if PLOT:
+    filename, ext = splitext(sys.argv[0])
+    myplot = PlotFenics(outputdirectory + filename + str(freq))
+    MPI.barrier(mpicomm)
+    myplot.set_varname('b_target')
+    myplot.plot_vtk(b_target_fn)
+
 # observations:
 obspts = [[0.0, ii/10.] for ii in range(1,10)] + \
 [[1.0, ii/10.] for ii in range(1,10)] + \
 [[ii/10., 0.0] for ii in range(1,10)]
+#obspts = [[1.0, 0.0]]
 
 # define pde operator:
 if mpirank == 0:    print 'define wave pde'
@@ -71,10 +80,75 @@ wavepde = AcousticWave({'V':V, 'Vm':Vm})
 wavepde.timestepper = 'backward'
 wavepde.lump = True
 
+# source:
+if mpirank == 0:    print 'sources'
+srcloc = [[ii/10., 1.0] for ii in range(1,10,2)]
+#srcloc = [[0.0, 1.0]]
+Ricker = RickerWavelet(freq, 1e-10)
+Pt = PointSources(V, srcloc)
+src = dl.Function(V)
+srcv = src.vector()
+mysrc = [Ricker, Pt, srcv]
+
+# define objective function:
+wavepde.update({'a':a_target_fn, 'b':b_target_fn, \
+'t0':t0, 'tf':tf, 'Dt':Dt, 'u0init':dl.Function(V), 'utinit':dl.Function(V)})
 regul = LaplacianPrior({'Vm':Vm,'gamma':1e-4,'beta':1e-4, 'm0':1.0})
+waveobj = ObjectiveAcoustic(wavepde, mysrc, 'b', regul)
+obsop = TimeObsPtwise({'V':V, 'Points':obspts}, t0tf)
+waveobj.obsop = obsop
 
+# Save MAP point, or assemble Hessian at MAP point
+if SAVE_MAP:
+    # noisy data
+    if mpirank == 0:    print 'generate noisy data'
+    waveobj.solvefwd()
+    DD = waveobj.Bp[:]
+    noiselevel = 0.1   # = 10%
+    for ii, dd in enumerate(DD):
+        np.random.seed(11)
+        nbobspt, dimsol = dd.shape
+        sigmas = np.sqrt((dd**2).sum(axis=1)/dimsol)*noiselevel
+        rndnoise = np.random.randn(nbobspt*dimsol).reshape((nbobspt, dimsol))
+        DD[ii] = dd + sigmas.reshape((len(sigmas),1))*rndnoise
+    waveobj.dd = DD
+    if mpirank == 0:
+        np.save(outputdirectory + filename + str(freq) + '/dd.npy', np.array(DD))
+    waveobj.solvefwd_cost()
+    if mpirank == 0:
+        print 'noise misfit={}, regul cost={}, ratio={}'.format(waveobj.cost_misfit, \
+        waveobj.cost_reg, waveobj.cost_misfit/waveobj.cost_reg)
+    if PLOT:    myplot.plot_timeseries(waveobj.solfwd[2], 'pd', 0, skip, dl.Function(V))
 
-for t0tf, freq, skip in zip(T0TF, FREQ, SKIP):
+    # solve inverse problem
+    if mpirank == 0:    print 'Solve inverse problem'
+    waveobj.inversion(b_target_fn, b_target_fn, mpicomm)
+    if PLOT:
+        myplot.set_varname('b_MAP')
+        myplot.plot_vtk(waveobj.PDE.b)
+
+    # Save MAP point
+    fileout = dl.HDF5File(mpicomm, \
+    outputdirectory + filename + str(freq) + '/MAP.h5', 'w')
+    fileout.write(waveobj.PDE.b, 'b')
+
+else:
+    # Load MAP point
+    fileout = dl.HDF5File(mpicomm, \
+    outputdirectory + filename + str(freq) + 'MAP.h5', 'r')
+    fileout.read(waveobj.PDE.b, 'b')
+
+    # Load data
+    DDarr = np.load(outputdirectory + filename + str(freq) + '/dd.npy')
+    DD = []
+    for d in DDarr:
+        DD.append(d)
+    waveobj.dd = DD
+
+    # Compute gradient
+    waveobj.solvefwd_cost()
+    waveobj.solveadj_constructgrad()
+
     # Parallelism or hand-made parallelism?
     try:
         myrank = int(sys.argv[1])
@@ -98,57 +172,6 @@ for t0tf, freq, skip in zip(T0TF, FREQ, SKIP):
         myrange = xrange(Vm.dim())
         Hessfilename = outputdirectory + 'Hessian' + str(freq) + '.dat'
 
-    # source:
-    if mpirank == 0:    print 'sources'
-    srcloc = [[ii/10., 1.0] for ii in range(1,10,2)]
-    Ricker = RickerWavelet(freq, 1e-10)
-    Pt = PointSources(V, srcloc)
-    src = dl.Function(V)
-    srcv = src.vector()
-    mysrc = [Ricker, Pt, srcv]
-
-    obsop = TimeObsPtwise({'V':V, 'Points':obspts}, t0tf)
-    t0, t1, t2, tf = t0tf
-    wavepde.update({'a':a_target_fn, 'b':b_target_fn, \
-    't0':t0, 'tf':tf, 'Dt':Dt, 'u0init':dl.Function(V), 'utinit':dl.Function(V)})
-
-    # set up plots:
-    if PLOT:
-        filename, ext = splitext(sys.argv[0])
-        myplot = PlotFenics(outputdirectory + filename + str(freq))
-        MPI.barrier(mpicomm)
-        myplot.set_varname('b_target')
-        myplot.plot_vtk(b_target_fn)
-
-    # define objective function:
-    waveobj = ObjectiveAcoustic(wavepde, mysrc, 'b', regul)
-    waveobj.obsop = obsop
-
-    # noisy data
-    if mpirank == 0:    print 'generate noisy data'
-    waveobj.solvefwd()
-    DD = waveobj.Bp[:]
-    noiselevel = 0.1   # = 10%
-    for ii, dd in enumerate(DD):
-        np.random.seed(11)
-        nbobspt, dimsol = dd.shape
-        sigmas = np.sqrt((dd**2).sum(axis=1)/dimsol)*noiselevel
-        rndnoise = np.random.randn(nbobspt*dimsol).reshape((nbobspt, dimsol))
-        DD[ii] = dd + sigmas.reshape((len(sigmas),1))*rndnoise
-    waveobj.dd = DD
-    waveobj.solvefwd_cost()
-    if mpirank == 0:
-        print 'noise misfit={}, regul cost={}, ratio={}'.format(waveobj.cost_misfit, \
-        waveobj.cost_reg, waveobj.cost_misfit/waveobj.cost_reg)
-    if PLOT:    myplot.plot_timeseries(waveobj.solfwd[2], 'pd', 0, skip, dl.Function(V))
-
-    # solve inverse problem
-    if mpirank == 0:    print 'Solve inverse problem'
-    waveobj.inversion(b_target_fn, b_target_fn, mpicomm)
-    if PLOT:
-        myplot.set_varname('b_MAP')
-        myplot.plot_vtk(waveobj.PDE.b)
-
     # Assemble data Hessian
     if mpirank == 0:    print 'Assemble data misfit part of the Hessian'
     waveobj.alpha_reg = 0.0
@@ -167,10 +190,10 @@ for t0tf, freq, skip in zip(T0TF, FREQ, SKIP):
             Hessian[ii, global_cc] = val
     Hessian.assemblyBegin()
     Hessian.assemblyEnd()
+
     # Print Hessian to file
     if mpirank == 0:    print 'Print Hessian to file'
     myviewer = PETSc.Viewer().createBinary(Hessfilename, \
     mode='w', format=PETSc.Viewer.Format.NATIVE, comm=mpicomm)
     myviewer(Hessian)
 
-    break
