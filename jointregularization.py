1f20332a0724d02769780bacc2aa888b5a5d8eeb
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2016-10-27 00:30:52 -0500

    start VTV

--- jointregularization.py
+++ jointregularization.py
@@ -1,9 +1,10 @@
-import sys
-import numpy as np
+"""
+Define joint regularization terms
+"""
 
 from dolfin import inner, nabla_grad, dx, \
 Function, TestFunction, TrialFunction, assemble, \
-PETScKrylovSolver, assign
+PETScKrylovSolver, assign, sqrt, Constant
 from miscfenics import setfct
 
 
@@ -46,6 +47,10 @@ class Tikhonovab():
             self.cg = crossgradient({'Vm':V})
             self.cgparam = parameters['cg']
         else:   self.cgparam = -1.0
+
+    # For compatibility with Total Variation regularization
+    def isTV(self): return False
+    def isPD(self): return False
 
     def costab(self, ma_in, mb_in):
         """ ma_in, mb_in = Function(V) """
@@ -160,12 +165,14 @@ class crossgradient():
         """ ma_in, mb_in = Function(V) """
         setfct(self.a, ma_in)
         setfct(self.b, mb_in)
+        self.H = None
         return assemble(self.cost)  # this is global (in parallel)
 
     def gradab(self, ma_in, mb_in):
         """ ma_in, mb_in = Function(V) """
         setfct(self.a, ma_in)
         setfct(self.b, mb_in)
+        self.H = None
         return assemble(self.grad)
 
     def assemble_hessianab(self, a, b):
@@ -181,3 +188,90 @@ class crossgradient():
         assign(self.abhat.sub(0), self.ahat)
         assign(self.abhat.sub(1), self.bhat)
         return self.H * self.abhat.vector()
+
+
+class VTV():
+    """ Define Vectorial Total Variation regularization for 2 parameters """
+
+    def __init__(self, Vm, parameters=[]):
+        """ Vm = FunctionSpace for the parameters (m1, m2) """
+
+        self.parameters = {'k':1.0, 'eps':1e-2}
+
+        self.m1 = Function(Vm)
+        self.m2 = Function(Vm)
+        self.m1h = Function(Vm)
+        self.m2h = Function(Vm)
+        self.m12h = Function(Vm*Vm)
+        testm1, testm2 = TestFunction(Vm*Vm)
+        trialm1, trialm2 = TrialFunction(Vm*Vm)
+
+        # cost function
+        normm1 = inner(nabla_grad(self.m1), nabla_grad(self.m1))
+        normm2 = inner(nabla_grad(self.m2), nabla_grad(self.m2))
+        TVnormsq = normm1 + normm2 + Constant(self.eps)
+        TVnorm = sqrt(TVnormsq)
+        self.wkformcost = self.k * TVnorm * dx
+
+        # gradient
+        gradm1 = self.k/TVnorm*inner(nabla_grad(self.m1), nabla_grad(testm1))*dx
+        gradm2 = self.k/TVnorm*inner(nabla_grad(self.m2), nabla_grad(testm2))*dx
+        self.gradm = gradm1 + gradm2
+
+        # Hessian
+        H11 = self.k/TVnorm*(inner(nabla_grad(trialm1), nabla_grad(testm1)) - \
+        inner(nabla_grad(testm1),nabla_grad(self.m1))* \
+        inner(nabla_grad(self.m1), nabla_grad(trialm1))/TVnormsq)*dx
+        H12 = -self.k/(TVnorm*TVnormsq)*(inner(nabla_grad(testm1),nabla_grad(self.m1))* \
+        inner(nabla_grad(self.m2), nabla_grad(trialm2)))*dx
+        H21 = -self.k/(TVnorm*TVnormsq)*(inner(nabla_grad(testm2),nabla_grad(self.m2))* \
+        inner(nabla_grad(self.m1), nabla_grad(trialm1)))*dx
+        H22 = self.k/TVnorm*(inner(nabla_grad(trialm2), nabla_grad(testm2)) - \
+        inner(nabla_grad(testm2),nabla_grad(self.m2))* \
+        inner(nabla_grad(self.m2), nabla_grad(trialm2))/TVnormsq)*dx
+        self.hessian = H11 + H12 + H21 + H22
+        #TODO: need a preconditioner for Hessian
+
+
+    def update(self, parameters=[]):
+        """ Update parameters of regularization """
+
+        self.parameters.update(parameters)
+        self.k = self.parameters['k']
+        self.eps = self.parameters['eps']
+
+
+    def costab(self, m1, m2):
+        """ Compute value of cost function at (m1,m2) """
+
+        setfct(self.m1, m1)
+        setfct(self.m2, m2)
+        self.H = None
+        return assemble(self.wkformcost)
+
+
+    def gradab(self, m1, m2):
+        """ returns gradient at (m1,m2) as a vector """
+
+        setfct(self.m1, m1)
+        setfct(self.m2, m2)
+        self.H = None
+        return assemble(self.gradm)
+
+
+    def assemble_hessianab(self, m1, m2):
+        """ Assemble Hessian matrix for regularization """
+
+        setfct(self.m1, m1)
+        setfct(self.m2, m2)
+        self.H = assemble(self.hessian)
+
+
+    def hessianab(self, m1h, m2h):
+        """ m1h, m2h = Vector(V) """
+
+        setfct(self.m1h, m1h)
+        setfct(self.m2h, m2h)
+        assign(self.m12h.sub(0), self.m1h)
+        assign(self.m12h.sub(1), self.m2h)
+        return self.H * self.m12h.vector()
