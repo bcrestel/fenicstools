16bf37835dbdb714ad0d40dc62259f31ef93fb50
Benjamin Crestel <crestel@ices.utexas.edu>
2017-03-16 14:16:44 -0500

    test splitandassign in parallel--OK

--- Test/test_splitassign.py
+++ Test/test_splitassign.py
@@ -6,11 +6,11 @@ import dolfin as dl
 
 from fenicstools.linalg.splitandassign import *
 
-#@profile
+@profile
 def testsplitassign():
     mesh = dl.UnitSquareMesh(40,40)
     V1 = dl.FunctionSpace(mesh, "Lagrange", 1)
-    V2 = dl.FunctionSpace(mesh, "Lagrange", 2)
+    V2 = dl.FunctionSpace(mesh, "Lagrange", 1)
     V1V2 = V1*V2
     splitassign = SplitAndAssign(V1, V2, mesh.mpi_comm())
 
@@ -47,7 +47,7 @@ def testsplitassign():
 
 
 def testblockdiagonal():
-    mesh = dl.UnitSquareMesh(4,4)
+    mesh = dl.UnitSquareMesh(40,40)
     V1 = dl.FunctionSpace(mesh, "Lagrange", 2)
     test1, trial1 = dl.TestFunction(V1), dl.TrialFunction(V1)
     V2 = dl.FunctionSpace(mesh, "Lagrange", 2)
@@ -56,24 +56,30 @@ def testblockdiagonal():
     test12, trial12 = dl.TestFunction(V1V2), dl.TrialFunction(V1V2)
     bd = BlockDiagonal(V1, V2, mesh.mpi_comm())
 
-    print 'mass+mass'
+    mpirank = dl.MPI.rank(mesh.mpi_comm())
+
+    if mpirank == 0:    print 'mass+mass'
     M1 = dl.assemble(dl.inner(test1, trial1)*dl.dx)
     M2 = dl.assemble(dl.inner(test1, trial2)*dl.dx)
     M12bd = bd.assemble(M1, M2)
     M12 = dl.assemble(dl.inner(test12, trial12)*dl.dx)
     diff = M12bd - M12
-    print np.linalg.norm(diff.array())/np.linalg.norm(M12.array())
+    nn = diff.norm('frobenius')
+    if mpirank == 0:
+        print nn
 
-    print 'mass+2ndD'
+    if mpirank == 0:    print 'mass+2ndD'
     D2 = dl.assemble(dl.inner(dl.nabla_grad(test1), dl.nabla_grad(trial2))*dl.dx)
     M1D2bd = bd.assemble(M1, D2)
     tt1, tt2 = test12
     tl1, tl2 = trial12
     M1D2 = dl.assemble(dl.inner(tt1, tl1)*dl.dx + dl.inner(dl.nabla_grad(tt2),dl.nabla_grad(tl2))*dl.dx)
     diff = M1D2bd - M1D2
-    print np.linalg.norm(diff.array())/np.linalg.norm(M1D2.array())
+    nn = diff.norm('frobenius')
+    if mpirank == 0:
+        print nn
 
-    print 'wM+wM'
+    if mpirank == 0:    print 'wM+wM'
     u11 = dl.interpolate(dl.Expression("x[0]*x[1]"), V1)
     u22 = dl.interpolate(dl.Expression("11+x[0]+x[1]"), V2)
     M1 = dl.assemble(dl.inner(u11*test1, trial1)*dl.dx)
@@ -82,7 +88,9 @@ def testblockdiagonal():
     ua, ub = dl.interpolate(dl.Expression(("x[0]*x[1]", "11+x[0]+x[1]")), V1V2)
     M12 = dl.assemble(dl.inner(ua*tt1, tl1)*dl.dx + dl.inner(ub*tt2, tl2)*dl.dx)
     diff = M12bd - M12
-    print np.linalg.norm(diff.array())/np.linalg.norm(M12.array())
+    nn = diff.norm('frobenius')
+    if mpirank == 0:
+        print nn
 
 
 
