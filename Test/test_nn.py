9c17dec7b4cd9b000acb6fd4431830ec0db2fcc8
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2017-04-12 21:31:53 -0500

    minor

--- Test/test_nn.py
+++ Test/test_nn.py
@@ -4,6 +4,7 @@ Test joint regularization with Nuclear Norm
 
 import sys
 from dolfin import *
+from dolfin import MPI
 import numpy as np
 from fenicstools.jointregularization import NuclearNormSVD2D, NuclearNormformula
 from fenicstools.miscfenics import setfct
@@ -15,63 +16,77 @@ def test_cost(eps_in):
     NN = NuclearNormSVD2D(mesh, eps=eps_in)
     NN2 = NuclearNormformula(mesh, eps=eps_in)
 
-    print 'Test 1'
+    mpicomm = mesh.mpi_comm()
+    mpirank = MPI.rank(mpicomm)
+    if mpirank == 0:
+        print '-------------------------------------'
+        print 'Test cost functional. eps={}'.format(eps_in)
+        print '-------------------------------------'
+
+    if mpirank == 0:    print 'Test 1'
     m1 = Function(NN.V)
     m2 = Function(NN.V)
     nn = NN.costab(m1, m2)
     nn2 = NN2.costab(m1, m2)
-    print 'cost={}, err={}'.format(nn, np.abs(nn))
-    print 'cost2={}, err={}'.format(nn2, np.abs(nn2))
+    if mpirank == 0:
+        print 'cost={}, err={}'.format(nn, np.abs(nn))
+        print 'cost2={}, err={}'.format(nn2, np.abs(nn2))
 
-    print '\nTest 2'
+    if mpirank == 0:    print '\nTest 2'
     m1 = interpolate(Expression("x[0]"), NN.V)
     m2 = interpolate(Expression("x[1]"), NN.V)
     nn = NN.costab(m1, m2)
     nn2 = NN2.costab(m1, m2)
     tt = 2.0
-    print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
-    print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
+    if mpirank == 0:
+        print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
+        print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
     
-    print '\nTest 3'
+    if mpirank == 0:    print '\nTest 3'
     m1 = interpolate(Expression("x[0]*x[0]"), NN.V)
     m2 = interpolate(Expression("x[1]*x[1]"), NN.V)
     nn = NN.costab(m1, m2)
     nn2 = NN2.costab(m1, m2)
     tt = 2.0
-    print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
-    print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
+    if mpirank == 0:
+        print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
+        print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
     
-    print '\nTest 4'
+    if mpirank == 0:    print '\nTest 4'
     m1 = interpolate(Expression("x[0] + x[1]"), NN.V)
     m2 = interpolate(Expression("x[0] + x[1]"), NN.V)
     nn = NN.costab(m1, m2)
     nn2 = NN2.costab(m1, m2)
     tt = 2.0
-    print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
-    print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
+    if mpirank == 0:
+        print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
+        print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
 
-    print '\nTest 5'
+    if mpirank == 0:    print '\nTest 5'
     m1 = interpolate(Expression("x[0] + x[1]"), NN.V)
     m2 = interpolate(Expression("x[0] - x[1]"), NN.V)
     nn = NN.costab(m1, m2)
     nn2 = NN2.costab(m1, m2)
     tt = 2.0*np.sqrt(2.0)
-    print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
-    print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
+    if mpirank == 0:
+        print 'cost={}, err={}'.format(nn, np.abs(nn-tt)/tt)
+        print 'cost2={}, err={}'.format(nn2, np.abs(nn2-tt)/tt)
 
-    print '\nTest 6'
+    if mpirank == 0:    print '\nTest 6'
     m1 = interpolate(Expression("x[0]*x[0] + x[1]"), NN.V)
     m2 = interpolate(Expression("x[0] + x[1]*x[1]"), NN.V)
     nn = NN.costab(m1, m2)
     nn2 = NN2.costab(m1, m2)
-    print 'cost={}, cost2={}, err={}'.format(nn, nn2, np.abs(nn-nn2)/np.abs(nn2))
+    if mpirank == 0:
+        print 'cost={}, cost2={}, err={}'.format(nn, nn2, np.abs(nn-nn2)/np.abs(nn2))
 
-    print '\nTest 7'
+    if mpirank == 0:    print '\nTest 7'
     m1 = interpolate(Expression("x[0]*x[0]*x[1]"), NN.V)
     m2 = interpolate(Expression("x[0]*x[1]*x[1]"), NN.V)
     nn = NN.costab(m1, m2)
     nn2 = NN2.costab(m1, m2)
-    print 'cost={}, cost2={}, err={}'.format(nn, nn2, np.abs(nn-nn2)/np.abs(nn2))
+    if mpirank == 0:
+        print 'cost={}, cost2={}, err={}'.format(nn, nn2, np.abs(nn-nn2)/np.abs(nn2))
 
 
 def test_grad(eps_in):
@@ -81,11 +96,21 @@ def test_grad(eps_in):
     m1h, m2h = Function(NN.V), Function(NN.V)
     H = [1e-4, 1e-5, 1e-6, 1e-7]
 
-    print 'Test 1'
+    mpicomm = mesh.mpi_comm()
+    mpirank = MPI.rank(mpicomm)
+    mpisize = MPI.size(mpicomm)
+    if mpirank == 0:
+        print '-------------------------------------'
+        print 'Test gradient. eps={}'.format(eps)
+        print '-------------------------------------'
+
+    if mpirank == 0:    print 'Test 1'
     m1 = Function(NN.V)
     m2 = Function(NN.V)
     grad = NN.gradab(m1, m2)
-    print '|grad|={}, err={}'.format(norm(grad), np.abs(norm(grad)))
+    normgrad = norm(grad)
+    if mpirank == 0:
+        print '|grad|={}, err={}'.format(normgrad, np.abs(normgrad))
 
     M1 = [interpolate(Expression("x[0] + x[1]"), NN.V),
     interpolate(Expression("x[0] * x[1]"), NN.V),
@@ -95,11 +120,11 @@ def test_grad(eps_in):
     interpolate(Expression("x[1]*x[1] + x[0]"), NN.V)]
     tt = 2
     for m1, m2 in zip(M1, M2):
-        print '\nTest {}'.format(tt)
+        if mpirank == 0:    print '\nTest {}'.format(tt)
         tt += 1
         grad = NN.gradab(m1, m2)
         for nn in range(5):
-            print '--direction {}'.format(nn)
+            if mpirank == 0:    print '--direction {}'.format(nn)
             direc1 = interpolate(Expression('1 + sin(n*pi*x[0])*sin(n*pi*x[1])',\
             n=nn), NN.V)
             direc2 = interpolate(Expression('1 + cos(n*pi*x[0])*cos(n*pi*x[1])',\
@@ -107,7 +132,7 @@ def test_grad(eps_in):
             assign(direc12.sub(0), direc1)
             assign(direc12.sub(1), direc2)
             direcderiv = grad.inner(direc12.vector())
-            print 'grad={}, '.format(direcderiv)
+            if mpirank == 0:    print 'grad={}, '.format(direcderiv)
 
             for hh in H:
                 setfct(m1h, m1)
@@ -127,11 +152,13 @@ def test_grad(eps_in):
                     err = np.abs(direcderiv-FDdirecderiv)/np.abs(direcderiv)
                 else:
                     err = np.abs(direcderiv-FDdirecderiv)
-                print '\th={}, fd={}, err={:.2e}'.format(hh, FDdirecderiv, err),
+                if mpirank == 0:
+                    print '\th={}, fd={}, err={:.2e}'.format(hh, FDdirecderiv, err),
                 if err < 1e-6:
-                    print '\t =>> OK!'
+                    if mpirank == 0:    print '\t =>> OK!'
                     break
-                else:   print ''
+                else:   
+                    if mpirank == 0:    print ''
 
 
 if __name__ == "__main__":
@@ -148,12 +175,6 @@ if __name__ == "__main__":
         eps = 1e-12
 
     if case_nb == 0:
-        print '-------------------------------------'
-        print 'Test cost functional. eps={}'.format(eps)
-        print '-------------------------------------'
         test_cost(eps)
     elif case_nb == 1:
-        print '-------------------------------------'
-        print 'Test gradient. eps={}'.format(eps)
-        print '-------------------------------------'
         test_grad(eps)
