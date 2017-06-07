be47992866f64cbca2777b935914901c8c716a7c
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2017-06-07 13:31:13 -0500

    start modification for compatibility with Fenics 2016.2.0

--- Test/test_lumpedsolver-parallel.py
+++ Test/test_lumpedsolver-parallel.py
@@ -1,8 +1,8 @@
-import dolfin as dl
-from fenicstools.linalg.lumpedmatrixsolver import LumpedMassMatrixPrime
-
 """ Assemble the matrix for the derivative of the lumped mass matrix
 Used to be run in parallel for debugging """
+
+import dolfin as dl
+from fenicstools.linalg.lumpedmatrixsolver import LumpedMassMatrixPrime
 
 mesh = dl.UnitSquareMesh(20,20)
 Va = dl.FunctionSpace(mesh, 'Lagrange', 2)
--- Test/test_mixedfunctionspace.py
+++ Test/test_mixedfunctionspace.py
@@ -4,26 +4,43 @@ Goal is to test how mixed function space behaves in Fenics
 from dolfin import *
 import numpy as np
 
-mesh = UnitSquareMesh(20,20)
+mpicomm = mpi_comm_world()
+mpirank = MPI.rank(mpicomm)
 
-V = FunctionSpace(mesh, 'CG', 1)
-VV = V*V
-Vw = FunctionSpace(mesh, 'DG', 0)
-VwVw = Vw*Vw
+#mpicommmesh = mpicomm
+mpicommmesh = mpi_comm_self()
+mesh = UnitSquareMesh(mpicommmesh, 20,20)
 
-print 'check gradient of m=(m1,m2) with m1,m2 cst returns 0'
+try:
+    Vfem = FiniteElement('CG', mesh.ufl_cell(), 1)
+    V = FunctionSpace(mesh, Vfem)
+    VV = FunctionSpace(mesh, Vfem*Vfem)
+    Vwfem = FiniteElement('DG', mesh.ufl_cell(), 0)
+    Vw = FunctionSpace(mesh, Vwfem)
+    VwVw = FunctionSpace(mesh, Vwfem*Vwfem)
+except:
+    V = FunctionSpace(mesh, 'CG', 1)
+    VV = V*V
+    Vw = FunctionSpace(mesh, 'DG', 0)
+    VwVw = Vw*Vw
+
+if mpirank == 0:
+    print 'check gradient of m=(m1,m2) with m1,m2 cst returns 0'
 m = Function(VV)
 m1 = interpolate(Constant("1"), V)
 m2 = interpolate(Constant("2"), V)
 assign(m.sub(0), m1)
 assign(m.sub(1), m2)
 normgrad = assemble(inner(nabla_grad(m), nabla_grad(m))*dx)
-print normgrad
+if mpirank == 0:
+    print normgrad
 
-print 'same with m=(x,2.0); should be 1'
-m1 = interpolate(Expression("x[0]"), V)
+if mpirank == 0:
+    print 'same with m=(x,2.0); should be 1'
+m1 = interpolate(Expression("x[0]", degree=10), V)
 assign(m.sub(0), m1)
 assign(m.sub(1), m2)
 normgrad = assemble(inner(nabla_grad(m), nabla_grad(m))*dx)
-print normgrad
+if mpirank == 0:
+    print normgrad
 
